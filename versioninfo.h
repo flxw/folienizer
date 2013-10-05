59887bfde1f415dadf6a483ffad760dfa51c7e6f
flxw <felix.wolff@flxw.de>
2013-10-05 17:00:00 +0200

    Add version information

---
+++ versioninfo.h
@@ -0,0 +1,11 @@
+#ifndef VERSIONINFO_H
+#define VERSIONINFO_H
+
+# include <QString>
+
+# define APPNAME    "folienizer"
+# define APPVERSION "alpha"
+# define ORGNAME    "flxw"
+# define ORGDOMAIN  "flxw.de"
+
+#endif // VERSIONINFO_H
